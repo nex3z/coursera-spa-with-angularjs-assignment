44297ec7b31171ec02b7f6b7c9180073d7fbcf98
nex3z <litianxing9@gmail.com>
2016-11-27 10:46:40 +0800

    Add solution for module 1.

---
+++ module1-solution/app.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function ($scope) {
+
+  $scope.checkDishes = function () {
+    var num = countDishes($scope.dishes);
+    $scope.message = buildMessage(num);
+  };
+
+  function countDishes(dishes) {
+    var array = dishes.split(',');
+    var count = 0;
+    for (var idx in array) {
+      if (array[idx].trim().length != 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  function buildMessage(num) {
+    if (num === 0) {
+      return 'Please enter data first';
+    }
+    else if (num <= 3) {
+      return 'Enjoy!';
+    } else {
+      return 'Too much!';
+    }
+  }
+});
+
+})();
