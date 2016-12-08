5b8a8c960dc7c7e243ffe0c288801bd89f046131
nex3z <litianxing9@gmail.com>
2016-12-08 21:33:49 +0800

    Add solution for module 4.

---
+++ module4-solution/src/routes.js
@@ -0,0 +1,45 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+  .state('categoryList', {
+    url: '/category-list',
+    templateUrl: 'src/menuapp/templates/category-list.template.html',
+    controller: 'CategoryListController as cateLstCtrl',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories().then(function(response) {
+          return response.data;
+        });
+      }]
+    }
+  })
+  .state('items', {
+    url: '/items/{category}',
+    templateUrl: 'src/menuapp/templates/items.template.html',
+    controller: 'ItemsController as itemsCtrl',
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+        return MenuDataService.getItemsForCategory($stateParams.category).then(function(response) {
+          return response.data.menu_items;
+        });
+      }]
+    }
+  });
+
+
+}
+
+})();
